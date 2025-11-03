943b8cd7aca3bbe1e088388332d5156047fdf321
PeterHou0129 <allfine4086@icloud.com>
2025-11-03 20:14:51 +0800

    Commit intial files

---
+++ index.js
@@ -0,0 +1,34 @@
+let countHome = document.getElementById("count-home")
+let countGuest = document.getElementById("count-guest")
+let count1 = 0
+let count2 = 0
+
+function add1(){
+    count1 += 1
+    countHome.textContent = count1
+}
+
+function add2(){
+    count1 += 2
+    countHome.textContent = count1
+}
+
+function add3(){
+    count1 += 3
+    countHome.textContent = count1
+}
+
+function addg1(){
+    count2 += 1
+    countGuest.textContent = count2
+}
+
+function addg2(){
+    count2 += 2
+    countGuest.textContent = count2
+}
+
+function addg3(){
+    count2 += 3
+    countGuest.textContent = count2
+}
